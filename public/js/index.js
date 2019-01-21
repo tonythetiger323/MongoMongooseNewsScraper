0635312387819038c29f86bf1fe0440c532f5261
Tony Greeley <tonythetiger323@users.noreply.github.com>
2019-01-21 17:17:39 -0600

    reverted back to original js structure

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,37 @@
-$(() => {
+$(document).ready(() => {
+
+  //event for when Scrape New Articles Button is clicked
   $('#scrape').on('click', event => {
     event.preventDefault();
 
     $.getJSON('/api/scrape', scrapeResult => {
+
       if (scrapeResult.code === 'success') {
         $('#articlesScraped').modal('show');
         $('#noArticles').remove();
 
         $.getJSON('/api/articles', data => {
 
-          res.json(data);
-        }
-        ).then(() => {
-          location.reload();
-        }
+          data.forEach(element => {
 
-        );
+            $('#articles').append(`<div id="${element._id}" class="card text-white bg-dark mb-3"><div class="card-header">${element.title}<button data-id="${element._id}" type="button" class="btn btn-outline-info float-right save">Save</button></div><div class="card-body"><p class="card-text"><a href="${element.link}">${element.link}</a></p></div></div>`);
+          });
+        });
       }
+    });
+  });
 
-// $(document).ready(() => {
+  $(document).on('click', '.save', function (event) {
+    event.preventDefault();
 
-//   //event for when Scrape New Articles Button is clicked
-//   $('#scrape').on('click', event => {
-//     event.preventDefault();
+    const thisId = $(this).attr('data-id');
 
-//     $.getJSON('/api/scrape', scrapeResult => {
+    $.ajax({
+      method: "PUT",
+      url: `api/articles/${thisId}/update`
+    })
+      .then($(`#${thisId}`).remove());
 
-//       if (scrapeResult.code === 'success') {
-//         $('#articlesScraped').modal('show');
-//         $('#noArticles').remove();
+  })
 
-//         $.getJSON('/api/articles', data => {
-
-//           data.forEach(element => {
-
-//             $('#articles').append(`<div id="${element._id}" class="card text-white bg-dark mb-3"><div class="card-header">${element.title}<button data-id="${element._id}" type="button" class="btn btn-outline-info float-right save">Save</button></div><div class="card-body"><p class="card-text"><a href="${element.link}">${element.link}</a></p></div></div>`);
-//           });
-//         });
-//       }
-//     });
-//   });
-
-//   $(document).on('click', '.save', function (event) {
-//     event.preventDefault();
-
-//     const thisId = $(this).attr('data-id');
-
-//     $.ajax({
-//       method: "PUT",
-//       url: `api/articles/${thisId}/update`
-//     })
-//       .then($(`#${thisId}`).remove());
-
-//   })
-
-// });
+});
